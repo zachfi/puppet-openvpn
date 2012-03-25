a935dc0731483d1c28b207221ee489fbb80b5bd3
Zach Leslie <zach@puppetlabs.com>
2012-03-25 13:27:25 -0700

    add tests

---
+++ tests/client.pp
@@ -0,0 +1,8 @@
+openvpn::client {
+  "node_${hostname}_dc1":
+    server => "vpn.dc1.example.com",
+    cert   => "node_${hostname}";
+  "node_${hostname}_office":
+    server => "vpn.office.example.com",
+    cert   => "node_${hostname}";
+}
---
+++ tests/server.pp
@@ -0,0 +1,15 @@
+class { "openvpn::server":
+  server => "10.0.0.0 255.255.255.0",
+  route  => [
+    "10.0.1.0 255.255.255.0",
+    "10.0.2.0 255.255.255.0",
+    "10.0.3.0 255.255.255.0",
+    ],
+  dns => '10.0.0.10',
+  crl => 'mysite/crl.pem',
+}
+
+openvpn::server::csc {
+  "srv1.example.com":
+    content => "ifconfig-push 10.0.0.50 10.0.0.51",
+}
