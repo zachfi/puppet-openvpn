9baa75607a87a21363419aba263a6f29de124674
Gaudenz Steinlin <gaudenz.steinlin@cirrax.com>
2013-02-07 09:41:27 +0100

    Paths for OpenBSD

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,10 +8,17 @@ class openvpn::params {
     'freebsd': {
       $openvpn_dir  = '/usr/local/etc/openvpn'
       $package_name = 'security/openvpn'
+      $openssl      = '/usr/bin/openssl'
+    }
+    'openbsd': {
+      $openvpn_dir  = '/etc/openvpn'
+      $package_name = 'openvpn'
+      $openssl      = '/usr/sbin/openssl'
     }
     default: {
       $openvpn_dir  = '/etc/openvpn'
       $package_name = 'openvpn'
+      $openssl      = '/usr/bin/openssl'
     }
   }
 
