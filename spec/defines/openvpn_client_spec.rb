2c0f5cc2944a066d1e69188b9ed03fa9dc26fc84
Zach Leslie <xaque208@gmail.com>
2018-02-10 19:31:45 -0800

    Update from xaque208 modulesync_config

--- spec/defines/openvpn_client_spec.rb
+++ spec/defines/openvpn_client_spec.rb
@@ -7,6 +7,7 @@ describe 'openvpn::client' do
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) { facts }
+
       it { is_expected.to contain_class('openvpn') }
 
       case facts[:osfamily]
