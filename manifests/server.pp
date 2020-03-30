9f4bf47837193b96a1993304cc25d3479cbbda11
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2020-03-30 19:36:56 +0200

    add parameter for setup of compression
    in openvpn 2.4 a new parameter 'compress' was introduces.
    To use the deprecated comp-lzo setting, set the compress
    parameter to 'legacy' (this is the default). To disable
    compression, set to ''.

--- manifests/server.pp
+++ manifests/server.pp
@@ -40,6 +40,7 @@ class openvpn::server (
   $topology                 = 'subnet',
   $custom_options           = [],
   $ccd                      = 'ccd',
+  $compress                 = 'legacy',
 ) {
 
   include openvpn
