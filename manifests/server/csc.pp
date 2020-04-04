8415c0547403a16555b22ad868886b1bef4a3a47
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2020-04-04 16:34:44 +0200

    use template for csc creation, also adds parameters for csc

--- manifests/server/csc.pp
+++ manifests/server/csc.pp
@@ -1,9 +1,13 @@
 # CSC: Client specific configuration
 define openvpn::server::csc (
-  Optional[String] $content = undef,
-  String           $owner   = 'root',
-  Integer          $group   = 0,
-  String           $mode    = '0640',
+  Optional[String] $content                    = undef,
+  String           $owner                      = 'root',
+  Integer          $group                      = 0,
+  String           $mode                       = '0640',
+  Optional[String] $ifconfig_push              = undef,
+  String           $ifconfig_push_netmask      = '255.255.255.0',
+  Optional[String] $ifconfig_ipv6_push         = undef,
+  String           $ifconfig_ipv6_push_netmask = '',
 ) {
 
   include openvpn
@@ -15,6 +19,6 @@ define openvpn::server::csc (
     owner   => $owner,
     group   => $group,
     mode    => $mode,
-    content => $content,
+    content => template('openvpn/csc.erb'),
   }
 }
