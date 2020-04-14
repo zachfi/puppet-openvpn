95b540b62cb381db46659109577dfd658a864036
Andrew Teixeira <teixeira@broadinstitute.org>
2020-04-14 12:22:56 -0400

    Absolutepath

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@
 #
 class openvpn (
   String $openvpn_dir,
-  String $openvpn_path,
+  Stdlib::Absolutepath $openvpn_path,
   String $package_name,
   Boolean $manage_service,
   Boolean $manage_systemd_unit,
